55ad5b84bce5472d9e2528cb1b8d0d18a57f9b6c
LRM-00018\Shashank Kankatti <shashank.kankatti@leftrightmind.com>
2023-11-23 13:29:35 +0530

    no message

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
- */
+
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
-/**
- *
- * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
- *
- * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
- * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
- * Let's keep the sky-high status of this song untouched!
- *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
- * Choose a heart-touching track! 🎵 ❤️
- *
- * Listen here: https://youtu.be/X0MDALpV29s
- *
- */
+
 $(document).on('click', function(){
-    document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
+   var audio = document.getElementById("my_audio");
+    // Play the audio
+    audio.play();
 });
 
 // Set the date we're counting down to
@@ -85,11 +71,4 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
